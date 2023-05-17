01f0023bbc9ed46f0a6c69ff68cd26a5f0c0a948
Roman2k29 <133137409+Roman2k29@users.noreply.github.com>
2023-05-17 19:29:20 +0300

    Add files via upload

--- jjj.py
+++ jjj.py
@@ -14,19 +14,22 @@ class GameSprite(sprite.Sprite):
         mw.blit(self.img, (self.rect.x, self.rect.y))
 class Player(GameSprite):
     def __init__(self, img, sizeX, sizeY, speed=0, id=1):
-        super().__init__(img, (maxX - sizeX)//2, maxY - sizeY, sizeX, sizeY, speed)  
+        if id ==1:
+            super().__init__(img, 0, maxY - sizeY, sizeX, sizeY, speed)  
+        else:
+            super().__init__(img, maxX - sizeX, maxY - sizeY, sizeX, sizeY, speed)  
         self.id = id
     def keyProcessing(self):
         keys = key.get_pressed()
         if self.id == 1:
-            if keys[K_UP]:
-                    self.rect.y -= self.speed 
-            if keys[K_DOWN]:
-                    self.rect.y += self.speed
-        else:
             if keys[K_w]:
                     self.rect.y -= self.speed 
             if keys[K_s]:
+                    self.rect.y += self.speed
+        else:
+            if keys[K_UP]:
+                    self.rect.y -= self.speed 
+            if keys[K_DOWN]:
                     self.rect.y += self.speed
 
  
@@ -35,13 +38,35 @@ class Player(GameSprite):
         elif self.rect.y < 0:
             self.rect.y = 0
     
+class Ball(GameSprite):
+    def __init__(self, img, x, y, sizeX, sizeY, speed=0):
+        super().__init__(img, x, y, sizeX, sizeY, speed)
+        self.speedX = (1 - 2*(randint(0,1))) * speed
+        self.speedY = (1 - 2*(randint(0,1))) * speed
+
+    def move(self):
+        self.rect.x += self.speedX
+        self.rect.y += self.speedY
+        if (self.rect.y <= 0):
+            self.speedY *= -1
+            return 3
+        if (self.rect.y + self.rect.height >= maxY):
+            self.speedY *= -1
+            return 4
+        if (self.rect.x <= 0):
+            self.speedX *= -1
+            return 1
+        if (self.rect.x + self.rect.width >= maxX):
+            self.speedX *= -1
+            return 2
+        return 0 
+            
+
 maxX = 1280
 maxY = 720
-
-wallColor = (150, 200, 150)
 mw = display.set_mode((maxX, maxY))
 display.set_caption('PINGPONG')
-
+bgColor = (255, 255, 255)
 #mixer.init()#инициализировать микшер
 #mixer.music.load('space.ogg')#загрузить фоновую музыку
 #mixer.music.play()#начать воспроизведение фоновой музыки
@@ -52,20 +77,27 @@ clock = time.Clock()
 #bg = GameSprite('galaxy.jpg', 0, 0, maxX, maxY)
 playerL = Player('red.png', 25, 100, 10, 1)
 playerR = Player('blue.png', 25, 100, 10, 2)
+ball = Ball('ball.png.', (maxX - 35)//2, (maxY - 35)//2, 35, 35, 5)
 
 font.init()###
 font_ = font.SysFont('ComicSans', 70)###
-
 victoryL = font_.render('Player 1 WON', True, (255, 0, 0))
 victoryR = font_.render('Player 2 WON', True, (255, 0, 0))
 
 gameRes = 0
 while game:
     if gameRes == 0:
+        mw.fill(bgColor)
         playerL.keyProcessing() 
-        playerR.keyProcessing() 
+        playerR.keyProcessing()
+        gameRes = ball.move()
+        if gameRes > 2: 
+            gameRes = 0
+        if sprite.collide_rect(ball, playerL) or sprite.collide_rect(ball, playerR):
+            ball.speedX *= -1
         playerL.reset()
         playerR.reset()
+        ball.reset()
     elif gameRes == 1:
         mw.blit(victoryL, (maxX//2, maxY//2))
     else:
@@ -74,4 +106,4 @@ while game:
         if e.type == QUIT:
             game = False
     display.update()
-    clock.tick(60)
+    clock.tick(90)
